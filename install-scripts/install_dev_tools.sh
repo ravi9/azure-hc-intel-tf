da926309f3798d80cfa8fc1c949ec4d39217ff5c
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2019-04-08 16:17:58 -0700

    Update install_dev_tools.sh

--- install-scripts/install_dev_tools.sh
+++ install-scripts/install_dev_tools.sh
@@ -4,10 +4,10 @@ set -ex
 # Install pre-reqs and development tools
 yum install -y epel-release
 yum groupinstall -y "Development Tools"
-yum install -y git wget pssh
+yum install -y git wget pssh sshpass nmap
 yum install -y numactl numactl-devel libxml2-devel byacc
 yum install -y pciutils lsof ethtool libnl3 libmnl tcsh
-yum install -y python-devel python-setuptools
+yum install -y python-devel python-setuptools python-pip
 yum install -y gtk2 atk cairo tcl tk
 yum install -y gcc-gfortran gcc-c++
 KERNEL=$(uname -r)
