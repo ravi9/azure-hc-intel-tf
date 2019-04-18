517b61fd5defbe917a52ca96cd3b15c2851c6931
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2019-04-17 23:22:10 -0700

    Add script to set ipob

--- azure-scripts/set-ipob.sh
+++ azure-scripts/set-ipob.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-#for line in `cat /home/kraman/nodenames.txt` 
-#do     
-
-#	echo $line
-#	ssh $line "sed -i 's/LOAD_EIPOIB=no/LOAD_EIPOIB=yes/g' /etc/infiniband/openib.conf; sudo /etc/init.d/openibd restart; ifconfig ib0 $(sed '/rdmaIPv4Address=/!d;s/.*rdmaIPv4Address="\([0-9.]*\)".*/\1/' /var/lib/waagent/SharedConfig.xml)/16"
+#Run this script as a sudo
 
 #done
 sed -i 's/LOAD_EIPOIB=no/LOAD_EIPOIB=yes/g' /etc/infiniband/openib.conf
-sudo /etc/init.d/openibd restart
+/etc/init.d/openibd restart
 ifconfig ib0 $(sed '/rdmaIPv4Address=/!d;s/.*rdmaIPv4Address="\([0-9.]*\)".*/\1/' /var/lib/waagent/SharedConfig.xml)/16
 
 
