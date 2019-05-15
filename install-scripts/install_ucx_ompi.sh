9d5d47d0e1b47bee7d95aaf8dc014475e810ea98
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2019-05-15 13:49:01 -0700

    Update to ucx1.5.1 and ompi4.0.1

--- install-scripts/install_ucx_ompi.sh
+++ install-scripts/install_ucx_ompi.sh
@@ -10,30 +10,30 @@ INSTALL_PREFIX=/opt
 mkdir -p /tmp/mpi
 cd /tmp/mpi
 
-# UCX 1.5.0
-wget https://github.com/openucx/ucx/releases/download/v1.5.0/ucx-1.5.0.tar.gz
-tar -xvf ucx-1.5.0.tar.gz
-cd ucx-1.5.0
-./contrib/configure-release --prefix=${INSTALL_PREFIX}/ucx-1.5.0 && make -j 40 && make install
+# UCX 1.5.1
+wget https://github.com/openucx/ucx/releases/download/v1.5.1/ucx-1.5.1.tar.gz
+tar -xvf ucx-1.5.1.tar.gz
+cd ucx-1.5.1
+./contrib/configure-release --prefix=${INSTALL_PREFIX}/ucx-1.5.1 && make -j 8 && make install
 cd ..
 
-# OpenMPI 4.0.0
-wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.0.tar.gz
-tar -xvf openmpi-4.0.0.tar.gz
-cd openmpi-4.0.0
-./configure --prefix=${INSTALL_PREFIX}/openmpi-4.0.0 --with-ucx=${INSTALL_PREFIX}/ucx-1.5.0 --enable-mpirun-prefix-by-default && make -j 40 && make install
+# OpenMPI 4.0.1
+wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.1.tar.gz
+tar -xvf openmpi-4.0.1.tar.gz
+cd openmpi-4.0.1
+./configure --prefix=${INSTALL_PREFIX}/openmpi-4.0.1 --with-ucx=${INSTALL_PREFIX}/ucx-1.5.1 --enable-mpirun-prefix-by-default && make -j 8 && make install
 cd ..
 
 cd && rm -rf /tmp/mpi
 
-ompi_path="export PATH=/opt/openmpi-4.0.0/bin:$PATH
-export LD_LIBRARY_PATH=/opt/openmpi-4.0.0/lib:$LD_LIBRARY_PATH
-export MANPATH=/opt/openmpi-4.0.0/share/man:$MANPATH
-export MPI_BIN=/opt/openmpi-4.0.0/bin
-export MPI_INCLUDE=/opt/openmpi-4.0.0/include
-export MPI_LIB=/opt/openmpi-4.0.0/lib
-export MPI_MAN=/opt/openmpi-4.0.0/share/man
-export MPI_HOME=/opt/openmpi-4.0.0"
+ompi_path="export PATH=/opt/openmpi-4.0.1/bin:$PATH
+export LD_LIBRARY_PATH=/opt/openmpi-4.0.1/lib:$LD_LIBRARY_PATH
+export MANPATH=/opt/openmpi-4.0.1/share/man:$MANPATH
+export MPI_BIN=/opt/openmpi-4.0.1/bin
+export MPI_INCLUDE=/opt/openmpi-4.0.1/include
+export MPI_LIB=/opt/openmpi-4.0.1/lib
+export MPI_MAN=/opt/openmpi-4.0.1/share/man
+export MPI_HOME=/opt/openmpi-4.0.1"
 
 echo -e "$ompi_path" >> /mnt/shared/setenv
 
